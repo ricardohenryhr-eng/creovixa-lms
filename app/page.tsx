827f890d171fc904e2635ce9d0d1339b8ba35907
ricardohenryhr-eng <ricardohenry.hr@gmail.com>
2026-09-18 01:58:31 -0400

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>Creovixa LMS</h1>
+      <p>Welcome to Creovixa Learning Management System.</p>
+    </main>
+  );
+}
